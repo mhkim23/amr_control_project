e9387a8ea7e7b6ef81d1721601176cc188fa42e9
Jeong Min Han <hanjm113@gmail.com>
2024-01-25 15:53:00 +0900

    update move

--- src/turtlebot3_move/src/turtlebot3_moving.py
+++ src/turtlebot3_move/src/turtlebot3_moving.py
@@ -1,92 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
-from nav_msgs.msg import Odometry
-from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-from math import degrees, sqrt, pi, atan2
-
-# define pi, 2pi, pi/2
-_2PI = 2.0 * pi
-_PI  = 1.0 * pi
-_R   = 0.5 * pi
-
-# Turtlebot3 Specification
-MAX_LIN_SPEED =  0.22
-MAX_ANG_SPEED =  2.84
-
-# make default speed of linear & angular
-LIN_SPD = MAX_LIN_SPEED * 0.125
-ANG_SPD = MAX_ANG_SPEED * 0.125
-
-class MoveTB3():
-
-    def __init__(self):  
-        rospy.Subscriber('/tb3pose', Pose, self.get_pose)
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)   
-        self.tb3pose  = self.org = Pose()
-                 
-    def get_pose(self, msg):
-        self.tb3pose = msg
-            
-    def update_org(self):
-        self.org = self.tb3pose
-                
-    def elapsed_dist(self):
-        return sqrt(pow((self.tb3pose.x - self.org.x), 2) + pow((self.tb3pose.y - self.org.y), 2))
-            
-    def straight(self, distance): 
-        tw = Twist()    
-        self.update_org()
-        print("start from (%s, %s)" %(round(self.org.x, 2), round(self.org.y, 2)))       
-        
-        if distance >= 0:   # +distance
-            tw.linear.x =  LIN_SPD
-        else:               # -distance
-            tw.linear.x = -LIN_SPD
-                        
-        self.pub.publish(tw)        
-        while self.elapsed_dist() < abs(distance):  pass
-            #print "%s(m) of %s(m)" %(round(self.elapsed_dist(),2), round(abs(distance),2))
-        
-        tw.linear.x = 0;    self.pub.publish(tw)
-        print("stop to (%s, %s)." %(round(self.tb3pose.x, 2), round(self.tb3pose.y, 2)))   
-        
-    def elapsed_angle(self):
-        return abs(self.tb3pose.theta - self.org.theta)
-        
-    def rotate(self, angle):
-        tw = Twist()
-        self.update_org()
-        print("start from: %s" %(round(degrees(self.org.theta), 2)))
-        
-        if angle >= 0:	# angle(+): rotate left(ccw)
-            tw.angular.z =  ANG_SPD;
-        else:			# angle(-): rotate right(cw)
-            tw.angular.z = -ANG_SPD;
-            
-        self.pub.publish(tw)
-        while self.elapsed_angle() < abs(angle):    pass
-            # print "%s of %s" %(round(degrees(self.elapsed_angle()),2) ,round(degrees(abs(angle)),2))
-            
-        tw.angular.z =  0;  self.pub.publish(tw)
-        print("stop to   : %s" %(round(degrees(self.tb3pose.theta), 2)))
-        
-class MoveTo_XY():
-
-    def __init__(self):
-        self.move = MoveTB3()
-    
-    def move2xy(self, x, y, theta):
-        
-        angle = atan2(y, x)
-        dist  = sqrt(pow(x, 2) + pow(y, 2))
-        
-        print("rotate %s(deg), and than straight %s(m)" %(degrees(angle), dist))
-        
-        self.move.rotate(angle)
-        self.move.straight(dist)
-        self.move.rotate(-angle + theta)
+from math import pow, sqrt, atan, pi#, atan2
+from control_node import MovingInPolar
 
 # 좌표값 받아오는 함수
 #class GetCoordinate():
@@ -97,21 +14,98 @@ class MoveTo_XY():
 #     y = msg.y
 #     theta = msg.theta
 #     return x, y, theta
-    
-if __name__ == '__main__':
 
+"""
+   TurtleBot3(buger) MAX SPEED
+-----------------------------------
+MAX Linear  Speed: 0.22(meter /sec)
+MAX Angular Speed: 2.82(radians/sec)
+"""
+
+MAX_LIN_X = 0.22
+MAX_ANG_Z = 2.82
+
+class Coord():
+
+    def __init__(self):
+        rospy.Subscriber('error_range', MovingInPolar, self.callbackfunc)
+        self.pose = MovingInPolar()
+
+    def callbackfunc(self, msg):
+        self.pose = msg
+        if(self.pose.status == True):
+            self.pose.psi1 = 0
+            self.pose.psi2 = 0
+            self.pose.movement = 0
+    
+class MoveBot:
+
+    def __init__(self, psi1, psi2, dist):
+        rospy.init_node('turtlebot_controller', anonymous=True)
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(10)
+        self.coord = Coord()
+        
+        self.lin_x = MAX_LIN_X / 2
+        self.ang_z = MAX_ANG_Z / 2
+        
+        self.fstDir = 1
+        self.sndDir = 1;
+        self.distDir = 1;
+    
+        self.fstAngle = self.coord.psoe.psi1
+        self.dist = self.coord.psoe.movenet
+        self.sndAngle = self.coord.psoe.psi2
+
+        #CW : Positive, CCW : Negative
+        self.fstAngle = -self.fstAngle * pi / 180
+        self.sndAngle = -self.sndAngle * pi / 180; 
+    
+        if(self.fstAngle < 0):
+            self.fstDir = -1;
+        if(self.sndAngle < 0):
+            self.sndDir = -1;  
+        if(self.dist <0):
+            self.distDir = -1;
+    
+    def move2goal(self):
+        
+        timeFstAngle = self.fstAngle / self.ang_z * self.fstDir
+        timeDist = self.dist / self.lin_x * self.distDir
+        timeSndAngle = self.sndAngle / self.ang_z * self.sndDir
+
+        twist = Twist()
+
+        #rotate psi1
+        twist.angular.z = self.ang_z * self.fstDir
+        time2end = rospy.Time.now() + rospy.Duration(timeFstAngle)
+        while(rospy.Time.now() < time2end):
+            self.pub.publish(twist)
+            self.rate.sleep()
+        twist.angular.z = 0
+        self.pub.publish(twist)
+
+        #move dist
+        twist.linear.x = self.lin_x * self.distDir
+        time2end = rospy.Time.now() + rospy.Duration(timeDist)
+        while(rospy.Time.now() < time2end): 
+            self.pub.publish(twist)
+            self.rate.sleep()
+    
+        twist.linear.x = 0
+        self.pub.publish(twist)
+
+        #rotate psi2
+        twist.angular.z = self.ang_z * self.sndDir
+        time2end = rospy.Time.now() + rospy.Duration(timeSndAngle)
+        while(rospy.Time.now() < time2end): 
+            self.pub.publish(twist)
+            self.rate.sleep()
+        twist.angular.z = 0
+        self.pub.publish(twist)
+
+if __name__ == '__main__':
     try:
-        rospy.init_node('move_to_xy', anonymous = True)        
-        xy = MoveTo_XY()
-        #coord = GetCoordinate()
-        
-        while not rospy.is_shutdown():
-            dist_x = float(input("input distance x(m): "))
-            dist_y = float(input("input distance y(m): "))
-            theta = float(input("input theta(deg): "))
-            # dist_x, dist_y, theta = coord.callbackfunc()
-            xy.move2xy(dist_x, dist_y, theta)
-            
-        rospy.spin()
-        
-    except rospy.ROSInterruptException:  pass
+        move = MoveBot()
+        move.move2goal()
+    except rospy.ROSInterruptException:   pass
